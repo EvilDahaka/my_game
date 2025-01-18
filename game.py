67feaba3d0531b040d5911d4720822051dc1d651
Kompik.Store <ctrax.xaxa@gmail.com>
2025-01-18 18:35:43 +0200

    update

--- game.py
+++ game.py
@@ -1,12 +1,16 @@
+from panda3d.core import *
 from direct.showbase.ShowBase import ShowBase
+from mapmanager import Mapmanager
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        self.model=loader.loadModel('models/environment')
-        self.model.reparentTo(render)
+        self.model=self.loader.loadModel('characterlowpoly2.obj')
+        self.model.reparentTo(self.render)
         self.model.setScale(0.1)
         self.model.setPos(-2, 25, -3)
         base.camLens.setFov(90)
+
+    
 
 base=ShowBase()
 
---
+++ mapmanager.py
@@ -0,0 +1,12 @@
+class Mapmanager():
+    def __init__(self):
+        self.model = 'characterlowpoly2.obj'
+        #self.texture
+        self.color = (0.2,0.2,0.35,1)
+    def startNew(self):
+        self.land = self.render.attachNewNode('Land')
+    def addBlock(self, position):
+        self.block = self.loader.loadModel(self.model)
+        self.block.setTexture(self.loader.loadTexture(self.texture))
+        self.block.setPos(position)
+        self.block.setColor(self.color)
