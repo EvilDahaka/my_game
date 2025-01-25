884f8078e8662aee63d497a4807b14e01c0e3bc0
EvilDahaka <davidlesho100608@gmail.com>
2025-01-25 14:54:27 +0200

    hero

--- game.py
+++ game.py
@@ -4,15 +4,13 @@ from mapmanager import Mapmanager
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        self.model=self.loader.loadModel('characterlowpoly2.obj')
+        self.model=self.loader.loadModel('13793_MaleHeroStanding_v1.obj')
         self.model.reparentTo(self.render)
         self.model.setScale(0.1)
         self.model.setPos(-2, 25, -3)
-        base.camLens.setFov(90)
+        self.camLens.setFov(90)
 
     
 
-base=ShowBase()
-
-
-base.run()
+game = Game()
+game.run()  # Запускає гру
--- mapmanager.py
+++ mapmanager.py
@@ -1,7 +1,7 @@
 class Mapmanager():
     def __init__(self):
-        self.model = 'characterlowpoly2.obj'
-        #self.texture
+        self.model = '20821_Cube_box_cover_v1.obj'
+        self.texture = 'Blank image.jpg'
         self.color = (0.2,0.2,0.35,1)
     def startNew(self):
         self.land = self.render.attachNewNode('Land')
