e170611a6b94129fcffb03123efb5c1a273bdea6
EvilDahaka <davidlesho100608@gmail.com>
2025-02-07 14:38:55 +0200

    control

--- game.py
+++ game.py
@@ -1,14 +1,16 @@
 from panda3d.core import *
 from direct.showbase.ShowBase import ShowBase
 from mapmanager import Mapmanager
+from hero import Hero
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        self.model=self.loader.loadModel('13793_MaleHeroStanding_v1.obj')
-        self.model.reparentTo(self.render)
-        self.model.setScale(0.1)
-        self.model.setPos(-2, 25, -3)
-        self.camLens.setFov(90)
+        self.land = Mapmanager(self)  # Передаємо об'єкт ShowBase до Mapmanager
+        self.camLens.setFov(90)  # Встановлюємо поле огляду камери
+        self.land.addBlock((0, 0, 0))
+        self.land.loadLand("land.txt")
+        self.hero = Hero((10, 8, 2), self.land)
+
 
     
 
--- hero.py
+++ hero.py
@@ -1,8 +1,10 @@
-class Hero():
-    def __init__(self,pos,land):
+
+class Hero:
+    def __init__(self, pos, land):
+        self.mode = True
         self.land = land
-        #self.hero = loader.loadModel()
-        self.hero.setColor(1, 0.5,0)
+        self.hero = loader.loadModel("characterlowpoly2.obj")
+        self.hero.setColor(1, 0.5, 0)
         self.hero.setScale(0.3)
         self.hero.setPos(pos)
         self.hero.reparentTo(render)
@@ -12,24 +14,55 @@ class Hero():
     def cameraBind(self):
         base.disableMouse()
         base.camera.setH(180)
+        base.camera.setP(0)
+        base.camera.setR(0)
         base.camera.reparentTo(self.hero)
         base.camera.setPos(0, 0, 1.5)
         self.cameraOn = True
 
     def cameraUp(self):
-        base.mouse.InterfaceNode.setPos(x,y,z)
+        pos = self.hero.getPos()
+        base.mouseInterfaceNode.setPos(-pos[0], -pos[1], -pos[2] - 3)
         base.camera.reparentTo(render)
         base.enableMouse()
-        self.cameraOn
-    def accept_events():
-        base.accept('n', self.turn_left)
-        base.accept('n'+' -repeat', self.turn_left)
+        self.cameraOn = False   
+
+    def accept_events(self):
+        base.accept('c', self.changeView)
+        base.accept('a',self.turn_left)
+        base.accept('a'+'-repeat',self.turn_left)
+        base.accept('d', self.turn_right)
+        base.accept('d'+'-repeat', self.turn_right)
 
     def turn_left(self):
-        self.hero.setH((self.hero.getH() 5) %360)
-        
+        self.hero.setH((self.hero.getH() + 5) % 360)
+    def turn_right(self):
+        self.hero.setH((self.hero.getH() - 5) % 360)
+
+
+
     def changeView(self):
         if self.cameraOn:
-            self.cameraUp
+            self.cameraUp()
         else:
-            self.cameraBind
+            self.cameraBind()
+
+
+       
+    def just_move(self, angle):
+        pass
+    def try_move(self, angle):
+        pass
+    def move_to(self, angle):
+        pass
+
+
+
+    def look_at(self, angle):
+        pass
+    def check_dir(self, angle):
+        pass
+
+
+
+    
